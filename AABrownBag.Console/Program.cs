47594c8d0e7f9a38b7a49be7a31fc324bdb4aa55
unknown <Sasa.Milovic@asglh-wl-10640.kingsway.asos.com>
2016-10-14 12:18:29 +0100

    adding

--- AABrownBag.Console/Program.cs
+++ AABrownBag.Console/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AABrownBag.Core;
+﻿using AABrownBag.Core;
 
 namespace AABrownBag.Console
 {
---
+++ AABrownBag/Controllers/GooglePriceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace AABrownBag.Controllers
+{
+    public class GooglePriceController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            // Yeah buddy, I went on holiday, and need you to finish this for me ASAP
+            // You should be able to use the google pricer class we use in the console
+            // app return the future price. Its all unit tested and working in prod,
+            // so this should be SUPER EASY! :)
+            return Ok("TODO");
+        }
+    }
+}
